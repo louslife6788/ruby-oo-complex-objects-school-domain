f33dd944874df932aad91ce82aaa205928ea3b84
luis leon <leonluis380@yahoo.com>
2021-06-18 21:46:11 -0400

    commit tree 00f393f1e51f3d81a1d7b04bb2756e9f7d8c4e4d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+
+class School 
+    attr_accessor :roster, :name
+
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+
+
+    end
+
+    def add_student(name, grade)
+        if @roster.include?(grade) 
+            @roster[grade] << name
+        else 
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+
+    def grade(grade)
+        @roster[grade]
+
+    end
+
+    def sort 
+        sorted = {}
+        @roster.each do |key, val|
+            sorted[key] = val.sort
+        end
+        sorted
+    end
+
+
+end
